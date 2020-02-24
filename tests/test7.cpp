e2f087122fbaba7e52b2fe74861e9488f0583c61
feiy <feiy@nvidia.com>
2020-02-24 15:23:02 +0800

    A better rendering of sponza

--- tests/test7.cpp
+++ tests/test7.cpp
@@ -33,28 +33,33 @@ int main()
 
 	int cubeWidth, cubeHeight, cubeChannels, isCube;
 	void *cube_pixels = dds_load("../data/sky_cube.dds", &cubeWidth, &cubeHeight, &cubeChannels, &isCube);
-	RGBACubemap cubemap(cubeWidth, cubeHeight, cube_pixels, false);
+	RGBACubemap cubemap(cubeWidth, cubeHeight, cube_pixels);
 	dds_free(cube_pixels);
 
 	int id_skybox = pt.add_cubemap(&cubemap);
 	TexturedSkyBox skybox(id_skybox);
 	pt.set_sky(&skybox);
-	pt.add_sunlight({ 1.0f, 1.0f, 1.0f }, 0.05, { 5000.0f, 5000.0f, 5000.0f });
+	pt.add_sunlight({ 0.3f, 1.0f, 0.2f }, 0.05, { 2000.0f, 2000.0f, 2000.0f });
 
 	glm::mat4x4 identity = glm::identity<glm::mat4x4>();
 
-	//SphereLight light1({ 0.0f, 200.0f, 0.0f }, 10.0f, { 10.0f, 10.0f, 10.0f });
-	//pt.add_geometry(&light1);
-
 	WavefrontObject obj(pt, "../data/sponza", "SponzaNoFlag.obj", identity);
 
+	glm::vec3 cam_pos = { 800.0f, 150.0f, 50.0f };
+	glm::vec3 cam_look_pos = { 0.0f, 300.0f, 0.0f };
+	glm::vec3 cam_dir = glm::normalize(cam_look_pos - cam_pos);
+	glm::vec3 light_pos = cam_pos - cam_dir * 50.0f;
+
 	pt.add_geometry(obj.get_geo());
-	pt.set_camera({ -1000.0f, 1000.0f, 0.0f }, { 0.0f, 350.0f, 0.0f }, { 0.0f, 1.0f, 0.0f }, 45.0f, 10.0f, 1400.0f);
+	pt.set_camera(cam_pos, cam_look_pos, { 0.0f, 1.0f, 0.0f }, 45.0f, 10.0f, 1500.0f);
+
+	//SphereLight light1(light_pos, 10.0f, { 100.0f, 100.0f, 100.0f });
+	//pt.add_geometry(&light1);
 
 	pt.trace(1000, 50);
 
 	unsigned char* hbuffer = (unsigned char*)malloc(view_width * view_height * 3);
-	target.to_host_srgb(hbuffer, 5.0f);
+	target.to_host_srgb(hbuffer, 2.5f);
 	stbi_write_png("test7.png", view_width, view_height, 3, hbuffer, view_width * 3);
 	free(hbuffer);
 
